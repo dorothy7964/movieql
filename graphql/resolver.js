7c3ff7a0b3da6340c70eb19e6b12f1142dad0beb
sample <dorothy_007@naver.com>
2019-08-16 23:44:33 +0900

    change axios

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,8 +1,17 @@
-import { getMovies } from "./db";
+import { getMovies, getById, addMovie, deleteMovie } from './db';
 
 const resolvers = {
     Query: {
-        movies:(_, {limit, rating}) => getMovies(limit, rating)
+        movies:(_, {limit, rating}) => getMovies(limit, rating),
+        movie: (_, {id}) => {
+            return getById(id);
+        }
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => {
+          return addMovie(name, score);
+        },
+        deleteMovie: (_ ,{id}) => deleteMovie(id),
     }
 };
 
